2fb4d549b7cb8739bdc0bef9917e37d855bffbd1
terman37 <terman37@yahoo.fr>
2020-03-26 16:16:38 +0100

    v0.2.1

--- tests/pygmo_test1.py
+++ tests/pygmo_test1.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import pygmo as pg
 import matplotlib.pyplot as plt
+import time
 from numba import jit
 
 rosenbrockdf = pd.read_csv('../data/rosenbrock.csv')
@@ -16,7 +17,7 @@ search_space = (-100, 100)
 
 class shifted_rosen:
 
-    def __init__(self, dim=50, search_space=(-100, 100), glob_opt=0):
+    def __init__(self, dim, search_space, glob_opt):
         self.lower_bound = search_space[0]
         self.upper_bound = search_space[1]
         self.dimension = dim
@@ -24,28 +25,20 @@ class shifted_rosen:
         self.glob_opt = glob_opt
 
     def fitness(self, x):
-        result = self.eval(x)
-        self.fitness_per_eval.append(result - self.glob_opt)
-        return [result]
-
-    def eval(self, x):
-        F = int(0)
-        z = np.empty(50)
+        F = 0
+        z = np.empty(self.dimension)
         for i in range(self.dimension - 1):
             z[i] = x[i] - rosenbrock[i] + 1
 
         for i in range(self.dimension - 2):
             F += 100 * ((z[i] ** 2 - z[i + 1]) ** 2) + (z[i] - 1) ** 2
         result = F + f_xstar
-        return result
+        return [result]
 
     def get_bounds(self):
         x_min = self.lower_bound * np.ones(self.dimension)
         x_max = self.upper_bound * np.ones(self.dimension)
         return x_min, x_max
-
-    def get_fitness_per_eval(self):
-        return self.fitness_per_eval
 
     def get_name(self):
         return "Shifted RosenBrock Function"
@@ -56,20 +49,34 @@ class shifted_rosen:
 
 DIM = 50
 pg.set_global_rng_seed(seed=37)
-# cur_x = np.random.uniform(-100, 100, (DIM))
-# print(cur_x)
 
 prob = pg.problem(shifted_rosen(DIM, search_space, f_xstar))
 print(prob)
-udp = prob.extract(shifted_rosen)
 
-algo = pg.algorithm(pg.pso(gen=100))
+# algo = pg.algorithm(pg.pso(gen=10))
+algo = pg.algorithm(pg.nlopt(solver='neldermead'))
 algo.set_verbosity(1)
 
-pop = pg.population(prob, 2)
-pop = algo.evolve(pop)
+pop = pg.population(prob, 1)
 
-log = algo.extract(pg.pso).get_log()
+t1 = time.time()
+pop = algo.evolve(pop)
+t2 = time.time()
+
+extract_algo = algo.extract(pg.pso)
+log = extract_algo.get_log()
+
+print("Algorithm: %s" % prob.get_name())
+# print("Parameters: %s" % options)
+print("Solution: ", pop.champion_x)
+print("Fitness: %f" % pop.champion_f[0])
+# print("Nb of functions evaluations: %d in %d iterations" % (result.nfev,result.nit))
+print("Nb of functions evaluations: %d in %d iterations" % (prob.get_fevals(), 0))
+# print("Stopping criterion: %s" % result.message)
+
+duration = t2 - t1
+print("computanional time: %.3f seconds" % duration)
+
 gbest = [x[2] for x in log]
 plt.plot(gbest)
 print(gbest)
