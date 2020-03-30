c4a1ffe2f435e95937cc168a9beb5cc37e4b6ca6
terman37 <terman37@yahoo.fr>
2020-03-30 23:24:06 +0200

    v0.5.4

--- 1-tsp_dj38/dj38_tsp_jmetalpy.py
+++ 1-tsp_dj38/dj38_tsp_jmetalpy.py
@@ -28,61 +28,42 @@ eval_fitness = []
 
 
 class myTSP(PermutationProblem):
-    """ Class representing TSP Problem. """
-
     def __init__(self, instance: str = None):
         super(myTSP, self).__init__()
 
         distance_matrix, number_of_cities = self.__read_from_file(instance)
-
         self.distance_matrix = distance_matrix
+        self.number_of_variables = number_of_cities
 
         self.obj_directions = [self.MINIMIZE]
-        self.number_of_variables = number_of_cities
         self.number_of_objectives = 1
         self.number_of_constraints = 0
 
     def __read_from_file(self, filename: str):
-        """
-        This function reads a TSP Problem instance from a file.
-        :param filename: File which describes the instance.
-        :type filename: str.
-        """
-
         if filename is None:
             raise FileNotFoundError('Filename can not be None')
-
         with open(filename) as file:
             lines = file.readlines()
             data = [line.lstrip() for line in lines if line != ""]
-
             dimension = re.compile(r'[^\d]+')
-
             for item in data:
                 if item.startswith('DIMENSION'):
                     dimension = int(dimension.sub('', item))
                     break
-
             c = [-1.0] * (2 * dimension)
-
             for item in data:
                 if item[0].isdigit():
-                    # modified from original int -> float
                     j, city_a, city_b = [float(x.strip()) for x in item.split(' ')]
                     c[2 * (int(j) - 1)] = city_a
                     c[2 * (int(j) - 1) + 1] = city_b
-
             matrix = [[-1] * dimension for _ in range(dimension)]
-
             for k in range(dimension):
                 matrix[k][k] = 0
-
                 for j in range(k + 1, dimension):
                     dist = math.sqrt((c[k * 2] - c[j * 2]) ** 2 + (c[k * 2 + 1] - c[j * 2 + 1]) ** 2)
                     dist = round(dist)
                     matrix[k][j] = dist
                     matrix[j][k] = dist
-
             return matrix, dimension
 
     def evaluate(self, solution: PermutationSolution) -> PermutationSolution:
